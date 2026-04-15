bb6655c035d8b0ab011ebad34bc5337c6aa8fec8
MICHAEL.TAYLOR <MICHAEL.TAYLOR@INFOTECH-67>
2026-04-14 21:10:58 -0500

    Discussed pointers and demoed basics of pointer assignment and dereferencing.

--- classwork/MovieLibrary/MovieLibrary/Program.cpp
+++ classwork/MovieLibrary/MovieLibrary/Program.cpp
@@ -274,8 +274,10 @@ Movie AddMovie()
     return movie;
 }
 
-// Arrays as parameters are always open arrays (undefined size)
-// Array size always follows array parameter
+/// @brief Adds a movie to the list
+/// @param movies Movie list
+/// @param size Size of list
+/// @return Index of added movie
 int AddMovie ( Movie movies[], int size )
 {
     //Get the movie details
@@ -310,10 +312,9 @@ void DeleteMovie(Movie& movie)
         movie.title = "";
 }
 
-//Arrays are pass by value
-//Array behaves like pass by reference (parameter and argument point to the same array in memory)
-// Since the parameter is pass by value you cannot change what the argument refers to (the array)
-// Arrays can never be the return type of a function
+/// @brief Delete a movie
+/// @param movies Movie list
+/// @param size Size of list
 void DeleteMovies(Movie movies[], int size)
 {
     //Determine movie to delete
@@ -331,6 +332,8 @@ void DeleteMovies(Movie movies[], int size)
     DeleteMovie(movies[index]);
 }
 
+/// @brief View movie details
+/// @param movie Movie to view
 void ViewMovie(Movie const& movie)
 {
     //Movie must exist
@@ -340,8 +343,7 @@ void ViewMovie(Movie const& movie)
     //Display movie details
     std::cout << "[" << movie.id << "] ";
     std::cout << movie.title << " (" << movie.releaseYear << ")" << std::endl;
-    std::cout << "Length (in minutes) " << movie.runLength << std::endl;
-    //std::cout << "Genre(s): " << movie.genres << std::endl;
+    std::cout << "Length (in minutes) " << movie.runLength << std::endl;    
     std::cout << "Genres: ";
     for (int index = 0; index < movie.genreCount; ++index)
     {
@@ -369,240 +371,42 @@ void ViewMovies(Movie movies[], int size)
     }
 }
 
-void ArrayDemo()
-{
-    const int MaximumValues = 10;
-    int values[MaximumValues];
-
-    // Accessing array elements
-    //    array_element ::= arr[index]
-    //    index ::= Ei
-    // You can read and write element values
-
-    // Index is specified inside brackets (integral)
-    // Index is always zero-based
-    //    Index 0 = Element 1 
-    //    Index 1 = Element 2
-    //    Index N = Element N+1
-    // Given an array of size M
-    //    There are M elements (1-M)
-    //    Index is 0 to M - 1
-    int thirdElement = values[2];
-
-    values[0] = 10;
-    values[1] = 20;
-    values[2] = 30;
-
-    //Iterate the elements in an array (0..<size)
-    // There is no way at runtime to correctly determine # of elements in the array
-    for (int index = 0; index < MaximumValues; ++index)
-    //int index = 0;
-    //for (int& value: values)
-    {
-        values[index] = index * 10;
-        //value = index++ * 10;
-    }
-
-    // for-range statement ::= for (T id : arr)
-    // Only works if the array is declared within the same function
-    //for (int index = 0; index < MaximumValues; ++index)
-    for (int value: values)
-    {
-        //int value = values[index];
-        //std::cout << values[index] << " ";
-        std::cout << value << " ";
-        value = 0;
-    }
-}
-
-void ArrayInitDemo()
-{
-    // Zero array initialization - initialize all elements in the array
-    //int daysInMonth[12] = {0};
-
-    // Full array initialization sets all elements in the array
-    int daysInMonth[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-    
-    //Init each element to specific value
-    /*daysInMonth[0] = daysInMonth[2] = daysInMonth[4] = daysInMonth[6] = daysInMonth[7] = 
-        daysInMonth[9] = daysInMonth[11] = 31;
-    daysInMonth[3] = daysInMonth[5] = daysInMonth[8] = daysInMonth[10] = 30;
-    daysInMonth[1] = 28;    */
-
-    //Implicit array sizing - when using an array initializater, size of array is optional
-    //  Use cases:
-    //    1. Size of array will never change for your life
-    //    2. string
-    std::string monthNames[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
-    //monthNames[0] = "Jan";
-    //monthNames[1] = "Feb";
-    //monthNames[2] = "Mar";
-    //monthNames[3] = "Apr";
-    //monthNames[4] = "May";
-    //monthNames[5] = "Jun";
-    //monthNames[6] = "Jul";
-    //monthNames[7] = "Aug";
-    //monthNames[8] = "Sep";
-    //monthNames[9] = "Oct";
-    //monthNames[10] = "Nov";
-    //monthNames[11] = "Dec";
-
-    for (int index = 0; index < 12; ++index)
-        std::cout << monthNames[index] << " has " << daysInMonth[index] << " days" << std::endl;
-
-    //Partial array initialization - only some element values are set, the rest are zero initialized
-    char name[100] = {'B', 'o', 'b'}; //"Bob"
-    char companyName[] = "My Company";
-
-    double rates[5] = {1.2, 3.4, 5.6, 7.8, 9.0};
-
-    int x = 100;
-    int numbers[10];
-    double r = 5.6;
-
-    // Off by one
-    //  Starting at 1 and therefore skipping first element
-    //  Indexing to size which is one after the last element
-    //Init the array
-    for (int index = 0; index < 10; ++index)
-        numbers[index] = index + 1;
-
-    for (int index = 0; index < 10; ++index)
-        std::cout << numbers[index] << " ";
-    std::cout << std::endl;
-}
-
-void CopyArray(int target[], int targetSize, int source[], int sourceSize)
-{
-    int size = (sourceSize <= targetSize) ? sourceSize : targetSize;
-
-    for (int index = 0; index < size; ++index)
-        target[index] = source[index];
-}
-
-bool CompareArray(int left[], int leftSize, int right[], int rightSize)
-{
-    if (leftSize != rightSize)
-        return false;
-
-    for (int index = 0; index < leftSize; ++index)
-        if (left[index] != right[index])
-            return false;
-
-    return true;
-}
-
-void DisplayArray(int values[], int size, int elementsPerLine, std::string delimiter)
-{
-    for (int index = 0; index < size; ++index)
-    {        
-        //Divides index by elements, when evenly divisible then print newline
-        if (index % elementsPerLine == 0 && index != 0)
-            std::cout << std::endl;
-
-        std::cout << values[index] << delimiter;
-    }
-
-    std::cout << std::endl;
-}
-
-void ArrayUsageDemo()
-{
-    //Things you can't do with array variables
-    int array1[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int array2[10] = {2,4, 6, 8, 9, 10, 12, 14, 16, 18 };
-
-    //1) Copy them
-    //array2 = array1;
-    CopyArray(array2, 10, array1, 5);
-
-    //2) Compare them
-    //bool areEqual = array1 == array2;
-    bool areEqual = CompareArray(array1, 10, array2, 10);
-
-    //3) Display them
-    //std::cout << array1 << std::endl;    
-    DisplayArray(array1, 10, 5, " ");
-
-    //Pre/postfix increment
-    int values[3] = {1, 2, 3};
-    int sum = 0;
-
-    int index = 0;
-    while (index < 3)
-    {
-        //Index uses prefix values[1], values[2], values[3?]
-        //sum += values[++index];
-        //sum += values[index++];  //values[0], values[1], values[2]
-
-        //Prefix to the element
-        //sum += ++values[index];
-        //index++;
-
-        //Equivalent to previous lines
-        sum += ++values[index++];
-    }
-}
-
-void DisplayTable(int table[][12], int size)
-{
-    for (int row = 0; row < size; ++row)
-        DisplayArray(table[row], 12, 12, " ");
-        //for (int col = 0; col < 12; ++col)
-        //    std::cout << table[row][col] << " ";
-
-    std::cout << std::endl;
-}   
-
-void MultiDimensionalArrayDemo()
-{
-    // 10 x 12 table of values 
-    const int MaxRows = 100;
-    const int MaxCols = 1000;
-    int multiplication[MaxRows][MaxCols] = {
-        { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
-        { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24 },
-        { 3, 6, 9, 12 },
-        { 4, 8, 12, 16 }
-    };  // 10 rows, 12 columns
-
-    // Row major (C++) - rows stored first, for row; for col
-    // Column major - cols stored first, for col; for row
-    for (int row = 0; row < MaxRows; ++row)
-        for (int col = 0; col < MaxCols; ++col)             
-            multiplication[row][col] = (row + 1) * (col + 1);
-
-    //DisplayTable(multiplication, MaxRows);
-}
-
 void main()
 {
-    //ArrayDemo();
-    //ArrayInitDemo();
-    //ArrayUsageDemo();
-    MultiDimensionalArrayDemo();
-
-    //TODO: Remove once no longer used
-    //Movie movie;
-    //Movie movie2, movie3, movie4; //Support more than 1 movie
     const int MaximumMovies = 100;
     Movie movies[MaximumMovies];         // Stores 100 movies
 
-    //Array declarations  T id [ Eci ];
-    //  Elements are the values stored in an array
-    //  Names are plural
-    //  All elements have same type
-    //  DO NOT USE to store unrelated values just to save variables
-    //  Value inside the brackets is the size, number of elements, in the array
-    //  Size must be:
-    //  1. a compile time constant 
-    //  2. > than 0
-    //  3. Integer
-    //  Array is NOT part of the type
-    // Other names for arrays: collection, vector, set, list, array
+    // Pointer = Memory address (hopefully in the heap)
+    int someValue = 100;  //Local variable, stored on call stack, 4 bytes in size
+    int* ptrValue;        //Local variable, stored on call stack, 4 or 8 bytes in size
+                          //Stores a memory address
+    ptrValue = &someValue; //Capture memory address of local variable
+    std::cout << "Pointer value = " << ptrValue << " "
+            << "Dereferenced value = " << *ptrValue << " "
+            << "Some Value address = " << &someValue << " "
+            << "Some Value value = " << someValue << " "
+            << std::endl;
 
-    // All arrays store their elements contiguously in memory
-    // Local variables are stored on stack so be careful of stack space
+    //T* T = int *ptrValue = int
+    *ptrValue = 200;
+
+    //Initialize your pointers
+    double* ptrRate = nullptr; // Preferred, always use this
+    *ptrRate = NULL; // Old C/C++ code, not recommended or used
+
+    // Naming pointers
+    //   prefix with 'ptr' or 'p'
+
+    //Dereferencing a pointer gives u access to the value stored at that pointer
+    // dereference_op ::= * E
+    // *ptrValue
+
+    // Type of a pointer is ALWAYS a memory address, int (32- or 64-bit)
+    // Derefencing a pointer, the type is the type it points to
+    //    T* means pointer to T means the dereferenced value is type T
+    //    *(int*) => int
+    //    * (double*) => double
+
 
     bool quit = false;
     while (!quit)
